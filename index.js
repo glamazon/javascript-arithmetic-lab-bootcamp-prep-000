2c88ad2564e0d342517f76bf0955bac67d59cf08
Peggy Sturman <glamazon5@gmail.com>
2018-09-20 19:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-function add() {
-  1 + 80
+function add(a,b){
+  return a + b;
 }
-
-fuction subtract() {
-  60 - 40
+ function subtract(a,b){
+  return a - b;
 }
-
-function multiply() {
-  2 * 3.4
+ function multiply(a,b){
+  return a * b;
 }
-
-function divide() {
-  5.0 / 2.5
+ function divide(a,b){
+  return a / b;
+}
+ function inc(n){
+  return n += 1;
+}
+ function dec(n){
+  return n -= 1;
+}
+ function makeInt(n){
+  return parseInt(n, 10);
+}
+ function preserveDecimal(n){
+  return parseFloat(n);
 }
